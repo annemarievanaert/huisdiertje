69f561a0e29e155071a42a76bfdae03638c2d851
annemarievanaert <101403220+annemarievanaert@users.noreply.github.com>
2022-03-14 13:20:34 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,44 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    honger += honger + 10
+    basic.showNumber(blij)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Confused)
-    bang += 5
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(honger)
-})
-let honger = 0
-honger = 0
-let bang = 0
+let blij = 0
+blij = 0
 basic.forever(function () {
-    honger += 1
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    if (honger == 20) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (honger == 35) {
-        basic.showIcon(IconNames.Confused)
-    }
-    if (honger == 50) {
-        basic.showIcon(IconNames.Angry)
-    }
-    if (honger == 60) {
-        basic.showIcon(IconNames.Skull)
-        honger = 0
-    }
-})
-basic.forever(function () {
-    if (bang == 15) {
-        basic.showIcon(IconNames.Angry)
-    }
-    if (bang == 20) {
-        basic.showIcon(IconNames.Skull)
+    blij += -2
+    if (blij < 1) {
+        basic.showIcon(IconNames.Happy)
     }
 })
